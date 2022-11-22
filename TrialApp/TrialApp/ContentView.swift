a69cbc2236db771a7e0c89565cc4af4639a19286
Guney Berkay <101819906+guneyberkayates@users.noreply.github.com>
2022-11-22 18:05:28 +0300

    Add files via upload

---
+++ TrialApp/TrialApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrialApp
+//
+//  Created by Güney Berkay  on 9.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrialApp/TrialApp/TrialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialAppApp.swift
+//  TrialApp
+//
+//  Created by Güney Berkay  on 9.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
